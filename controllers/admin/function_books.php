f7ff7bd5fbedfab99f5f368eed41ba1cda00b18b
Hilmi26 <hilmi.ts26@gmail.com>
2022-10-01 07:35:39 +0700

    add file

--- controllers/admin/function_books.php
+++ controllers/admin/function_books.php
@@ -5,7 +5,7 @@ include '../../config/connection.php';
 function display($table, $condition = ""){
 	global $conn;
 	$query = "SELECT * FROM ". $table . " " . $condition;
-	var_dump($query);
+	// var_dump($query);
 	$cek = mysqli_query($conn, $query);
 	return $cek;
 }
