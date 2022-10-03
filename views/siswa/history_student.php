10f1201a8fb0e1cd26cca6a8f59934dff82e80fc
Zacky Kurniawan <77685152+zackykurniawan@users.noreply.github.com>
2022-10-03 23:38:00 +0700

    logout

--- views/siswa/history_student.php
+++ views/siswa/history_student.php
@@ -151,7 +151,7 @@ include '../../config/connection.php';
                                         <div class="dropdown-divider"></div>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="auth-login-basic.html">
+                                        <a class="dropdown-item" href="../login/logout.php">
                                             <i class="bx bx-power-off me-2"></i>
                                             <span class="align-middle">Log Out</span>
                                         </a>
